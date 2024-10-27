bdc88a3e3848a5d543b0151569cdf330a1513a32
Theeyesign <sejupato@gmail.com>
2024-10-27 17:34:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function showHistory() {
     const month = document.getElementById("month").value;
     const year = document.getElementById("year").value;
     
-    // Lógica para mostrar la historia según la fecha
     let historyPart = "";
 
     if (day && month && year) {
@@ -24,9 +23,3 @@ function showHistory() {
     notes.innerHTML = historyPart;
 }
 
-// Esta función para mostrar la fecha
-document.getElementById("lettersInput").addEventListener("input", function() {
-    const dateDisplay = document.getElementById("date-display");
-    const date = new Date();
-    dateDisplay.textContent = `Día ${date.getDate()}, del mes ${date.getMonth() + 1}, del año ${date.getFullYear()}`;
-});
